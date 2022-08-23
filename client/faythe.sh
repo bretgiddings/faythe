99c97a279927b0651bb005163ac3a2fc6723e46d
bretgiddings <bretgiddings@gmail.com>
2022-08-23 20:36:55 +0100

    Ensure variables are quoted for bash 3.x

--- client/faythe.sh
+++ client/faythe.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-# Source from end of ~/.bashrc, ~/.zshrc or similar
-
 # (POSIX) shell script to implement tranparently signed ssh public keys
 # on a per-domain basis. Uncomment alias lines at end for simplest usage.
 # Note that you can still use /usr/bin/ssh if for any reason this then
 # interferes with your normal ssh
 
 _faythe_signkey() {
-	# no args, just let ssh/scp report
+
+	# no args, just let ssh/scp/sftp do its thing
 	if [ $# -eq 0 ]; then
 		return 0
 	fi
-
+ 
 	# read $HOME/.config/faythe/domains
 	# TODO - what should happen if this doesn't exist?
 	found=""
@@ -24,7 +23,7 @@ _faythe_signkey() {
 		fi
 	done < $HOME/.config/faythe/domains
 
-	if [ -z ${found} ]; then
+	if [ -z "${found}" ]; then
 		# no match so no need to check private key
 		return 0
 	fi
@@ -42,36 +41,38 @@ _faythe_signkey() {
 	fi
 
 	if [ -f "$cert" ]; then
-		valid=$( /usr/bin/ssh-keygen -Lf $cert | grep Valid: | sed -E 's/^(.* to )(.*)/\2/' )
+		valid=$( /usr/bin/ssh-keygen -Lf "$cert" | grep Valid: | sed -E 's/^(.* to )(.*)/\2/' )
 		if [ $( uname ) = "Darwin" ]; then	# mac
-			until=$( date -j -f '%FT%T' $valid +'%s' )
+			until=$( date -j -f '%FT%T' "$valid" +'%s' )
 		else
-			until=$( date -d $valid +'%s' )
+			until=$( date -d "$valid" +'%s' )
 		fi
 		now=$( date +'%s' )
-		if [ $now -lt $until ]; then
-			printf "+faythe: Current signed certificate valid until $valid\\n" >&2
+		if [ "$now" -lt "$until" ]; then
+			printf "+faythe: Current signed certificate valid until ${valid}\\n" >&2
 			return 0
 		else
-			printf "+faythe: Signed certificate expired $valid\\n" >&2
+			printf "+faythe: Signed certificate expired ${valid}\\n" >&2
 		fi
 	fi
 
 	printf '+faythe: Requesting signed certificate ...\n' >&2
-	output=$( /usr/bin/ssh -T ${login}@sshca.${domain} 2>/dev/null )
+	output=$( /usr/bin/ssh -T "${login}@sshca.${domain}" 2>/dev/null )
 
-	if [ -z $output ]; then
-		return 0
+	if [ -z "$output" ]; then
+		return 1
 	fi
 
-	key=$( printf $output | sed -E -n '/^ssh-(rsa|ed25519|ecdsa)-cert-v[[:digit:]]+@openssh.com AAA/p' )
+	key=$( printf "$output" | sed -E -n '/^ssh-(rsa|ed25519|ecdsa)-cert-v[[:digit:]]+@openssh.com AAA/p' )
+
+
 	if [ ! -z "$key" ]; then
-		printf "%s" $key > $cert
-		valid=$( /usr/bin/ssh-keygen -Lf $cert | grep Valid: | sed -E 's/^(.* to )(.*)/\2/' )
-		printf "+faythe: Wrote new key to %s file - valid until %s\\n" $cert $valid >&2
+		printf "%s" "$key" > "$cert"
+		valid=$( /usr/bin/ssh-keygen -Lf "$cert" | grep Valid: | sed -E 's/^(.* to )(.*)/\2/' )
+		printf "+faythe: Wrote new key to %s file - valid until %s\\n" "$cert" "$valid" >&2
 	else
 		printf '+faythe: Failed to update cert signed key.\n' >&2
-		printf $output
+		printf "$output"
 		return 1
 	fi
 
@@ -90,6 +91,13 @@ fscp() {
 	fi
 }
 
+fsftp() {
+	if _faythe_signkey $*; then
+		/usr/bin/sftp $*
+	fi
+}
+
 # uncomment for most transparent setup
 alias ssh=fssh
 alias scp=fscp
+alias sftp=fsftp
