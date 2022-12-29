ff8d9dc1138db81b0b343dc6bca99e92016fff48
bretgiddings <bretgiddings@gmail.com>
2022-12-29 11:35:39 +0000

    Modify installers to be more  automated

--- client/installer.sh
+++ client/installer.sh
@@ -50,25 +50,20 @@ if [ -z "$SSH_AUTH_SOCK" ]; then
 fi
 
 if [ ! -d ~/.ssh ]; then
-    echo "You don't have a .ssh directory - please create it using
-
+    echo "+ Creating ${HOME}/.ssh directory"
     mkdir ~/.ssh
     chmod 700 ~/.ssh
-"
-    exit 1
 fi
 
 if [ ! -f ~/.ssh/id_ed25519_$domain ]; then
-    echo "You don't have a id_ed25519_$domain keypair. Please create one using
+    echo "+ Running ssh-keygen - use a memorable passphrase and make a note of it."
 
-ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519_$domain
-"
-    exit 1
+    ssh-keygen -q -t ed25519 -f ~/.ssh/id_ed25519_$domain
 fi
 
 if ssh-keygen -p -f ~/.ssh/id_ed25519_$domain -N '' -P '' >/dev/null 2>&1; then
     echo "Your SSH key (~/.ssh/id_ed25519_$domain doesn't have a passphrase, please add one using
-
+ech
 ssh-keygen -p -f ~/.ssh/id_ed25519_$domain
 "
     exit 1
@@ -84,21 +79,18 @@ if [ $? = 1 ]; then
       extra=" --apple-use-keychain"
     fi
 
-    echo "Your SSH key hasn't been added to your SSH agent - please add using
-
-ssh-add -k${extra} $HOME/.ssh/id_ed25519_$domain
-"
-    exit 1
+    echo "+ Adding your key to your agent ... use the same passphrase as above."
+    ssh-add -k${extra} $HOME/.ssh/id_ed25519_$domain
 fi
 
 if [ ! -d ~/.config/faythe ]; then
-    echo "Creating ~/.confg/faythe directory."
+    echo "+ Creating ~/.confg/faythe directory."
     
     mkdir -p ~/.config/faythe
 fi
 
 if [ ! -f ~/.config/faythe/domains ]; then
-    echo "Creating ~/.config/faythe/domains for essex.ac.uk"
+    echo "+ Creating ~/.config/faythe/domains for essex.ac.uk"
     cat > ~/.config/faythe/domains <<EOF
 # Config file for SSH CA
 # format - line containing 3 fields
@@ -109,10 +101,10 @@ $domain $login ~/.ssh/id_ed25519_$domain
 EOF
 fi
 
-echo "Installing current version of faythe.sh"
+echo "+ Installing current version of faythe.sh"
 cp /tmp/faythe.sh ~/.config/faythe/faythe.sh
 
-echo "Checking your ~/.ssh/config file ..."
+echo "+ Checking your ~/.ssh/config file ..."
 
 sshConfig="# ssh basic config file for remote access
 
@@ -121,19 +113,21 @@ Host sshca.$domain sshgw.$domain sshenrol.$domain $noproxy
     User $login
     ProxyJump none
 
+Match Host !sshgw.$domain,!sshca.$domain,*.$domain !exec "ssh-keyscan -T 1 %h >~/.ssh/junk 2>&1"
+    ProxyJump ${login}@sshgw.$domain
+
 # anything else @ $domain, use standard settings
 Host *.$domain
     IdentityFile ~/.ssh/id_ed25519_$domain
     ForwardAgent yes
     User $login
-    ProxyJump ${login}@sshgw.$domain
 "
 if [ ! -f ~/.ssh/config ]; then
-    echo "Creating ~/.ssh/config ..."
+    echo "+ Creating ~/.ssh/config ..."
     echo "$sshConfig" > ~/.ssh/config
     chmod 0600 ~/.ssh/config
 else
-    echo "Please modify your config to include ...
+    echo "+ Please modify your config to include ...
 
 $sshConfig
 "
@@ -148,3 +142,38 @@ else
     echo "$trustedHostCA" >> ~/.ssh/known_hosts
     chmod 600 ~/.ssh/known_hosts
 fi
+
+# determine environment for . file stuff ...
+
+unameOut="$( uname -s )"
+case "$unameOut" in
+    Linux*)     osEnv=Linux
+                ;;
+    Darwin*)    osEnv=Mac
+                ;;
+    CYGWIN*)
+        osEnv=Cygwin
+        if [ ! -f $HOME/.bash_profile ]; then
+            touch $HOME/.bash_profile
+            chmod +x $HOME/.bash_profile
+        fi
+
+        if egrep -q '^. $HOME/.config/faythe/faythe.sh' $HOME/.bash_profile; then
+            echo "+ Custom script already in .bash_profile"
+        else
+            echo "+ Adding '. \${HOME}/.config/faythe/faythe.sh' to $HOME/.bash_profile"
+            printf -- "\n# Added by faythe installer\n. \${HOME}/.config/faythe/faythe.sh" >> $HOME/.bash_profile
+        fi
+
+        ;;
+    MINGW*)     osEnv=MinGw
+                ;;
+    *)          osEnv="UNKNOWN:${unameOut}"
+esac
+
+echo "+ Enrolling your SSH public key."
+/usr/bin/ssh ${login}@sshenrol.$domain "$(cat ${HOME}/.ssh/id_ed25519_${domain}.pub)"
+
+# 
+echo "+ Enabling new functions ..."
+exec $SHELL
--- client/windows-installer.ps1
+++ client/windows-installer.ps1
@@ -46,6 +46,7 @@ else {
 }
 
 if ( -not ( ( Get-Service ssh-agent | Select-Object -ExpandProperty StartType ) -eq "Automatic" ) ) {
+    #Start-Process -Verb RunAs -FilePath Powershell -ArgumentList '/c "Set-Service -Name ssh-agent -Startuptype Automatic"'
     Write-Host @"
 Your ssh-agent isn't set to run automatically - please open an administrative PowerShell window and run
 
@@ -56,7 +57,13 @@ Then re-run this script.
     return
 }
 else {
-    Write-Verbose "ssh-agent set to run OK."
+    Write-Verbose "ssh-agent set to run Automatically."
+}
+
+if ( -not ( ( Get-Service ssh-agent | Select-Object -ExpandProperty Status ) -eq "Running" ) ) {
+    Write-host "+ Attempting to start ssh-agent ..."
+    Start-Service ssh-agent
+    Start-Sleep -Seconds 1;
 }
 
 if ( -not ( ( Get-Service ssh-agent | Select-Object -ExpandProperty Status ) -eq "Running" ) ) {
@@ -74,22 +81,19 @@ else {
 }
 
 if ( -not ( Test-Path $HOME/.ssh -PathType Container ) ) {
-    Write-Host "Creating $HOME/.ssh folder."
-    New-Item -Path $HOME/.ssh -ItemType Container
+    Write-Host "+ Creating $HOME/.ssh folder."
+    New-Item -Path $HOME/.ssh -ItemType Container | Out-Null
 }
 else {
     Write-Verbose "$HOME/.ssh folder present."
 }
 
+$createdKeyPair = $false
+
 if ( -not ( Test-Path $HOME/.ssh/id_ed25519_$Domain ) ) {
-    Write-Host @"
-Please run:
-
-    ssh-keygen -t ed25519 -f $HOME/.ssh/id_ed25519_$Domain -C "$Login@$Domain faythe key"
-
-and use a strong memorable password. Then re-run this script.
-"@
-    return
+    $createdKeyPair = $true
+    Write-Host "+ Running ssh-keygen - use a memorable passphrase and make a note of it."
+    & ssh-keygen -q -t ed25519 -f "$HOME/.ssh/id_ed25519_$Domain" -C "$Login@$Domain faythe key"
 }
 else {
     Write-Verbose "id_ed25519_$Domain key present."
@@ -112,6 +116,11 @@ else {
 }
 
 $fingerprint = $( & $sshDir/ssh-keygen.exe -l -f $HOME/.ssh/id_ed25519_$Domain )
+
+if ( -not ( & $sshDir/ssh-add.exe -l | Select-String -SimpleMatch "$fingerprint" ) ) {
+    Write-Host "+ Adding your key to your agent ... use the same passphrase as above."
+    & ssh-add -k $HOME/.ssh/id_ed25519_$Domain
+}
 
 if ( -not ( & $sshDir/ssh-add.exe -l | Select-String -SimpleMatch "$fingerprint" ) ) {
     Write-Host @"
@@ -147,12 +156,12 @@ if ( ( Test-Path -Path $PROFILE -PathType Leaf ) ) {
     }
 }
 else {
-    Write-Host "Creating $PROFILE"
+    Write-Host "+ Creating powershell profile $PROFILE"
 
     $ProfileDir = Split-Path $PROFILE
 
     if ( -not ( Test-Path $ProfileDir -Type Container ) ) {
-        New-Item -Path $ProfileDir -ItemType Container 
+        New-Item -Path $ProfileDir -ItemType Container | Out-Null
     }
 
     @'
@@ -166,9 +175,9 @@ Set-Alias -Name sftp -value fstfp
 
 if ( -not ( Test-Path -Path $env:APPDATA/faythe/domains -PathType Leaf ) ) {
     if ( -not ( Test-Path -Path $env:APPDATA/faythe -PathType Container ) ) {
-        New-Item -Path $env:APPDATA/faythe -ItemType Container
+        New-Item -Path $env:APPDATA/faythe -ItemType Container | Out-Null
     }
-    Write-Host "Created $env:APPDATA/faythe/domains"
+    Write-Host "+ Created $env:APPDATA/faythe/domains."
     @"
 # Config file for SSH CA
 # format - line containing 3 fields
@@ -179,19 +188,19 @@ $Domain $Login ~/.ssh/id_ed25519_$Domain
 "@ | Out-File -FilePath $env:APPDATA/faythe/domains -Encoding ascii
 }
 
-Write-Host "Creating/updating $faythePS1"
+Write-Host "+ Creating/updating $faythePS1."
 
 if ( -not ( Test-Path $faythePS1 -PathType Leaf ) ) {
     Write-Host "${faythePS1}: file not found."
     return
 } 
 else {
-    New-Item $env:APPDATA/faythe -Type Directory -ErrorAction SilentlyContinue
-    Copy-Item $FaythePS1 $env:APPDATA/faythe/faythe.ps1
+    New-Item $env:APPDATA/faythe -Type Directory -ErrorAction SilentlyContinue | Out-Null
+    Copy-Item $FaythePS1 $env:APPDATA/faythe/faythe.ps1 | Out-Null
     Write-Verbose "Copied faythe.ps1 to $env:APPDATA/faythe"
 }
 
-Write-Host "Checking SSH config ..."
+Write-Host "+ Checking SSH config ..."
 
 $sshConfigFile = @"
 # ssh basic config file for remote access
@@ -209,12 +218,12 @@ Host *.$Domain
 "@
 
 if ( -not ( Test-Path -Path "${HOME}/.ssh" -PathType Container ) ) {
-    Write-Host "Creating ${HOME}/.ssh"
-    New-Item -Path "${HOME}/.ssh" -ItemType Container
+    Write-Host "Creating ${HOME}/.ssh."
+    New-Item -Path "${HOME}/.ssh" -ItemType Container | Out-Null
 }
 
 if ( -not ( Test-Path -Path "${HOME}/.ssh/config" ) ) {
-    Write-Host "Creating ${HOME}/.ssh/config"
+    Write-Host "+ Creating ${HOME}/.ssh/config."
     $sshConfigFile | Out-File -FilePath ${HOME}/.ssh/config -Encoding ascii
 }
 else {
@@ -227,7 +236,7 @@ $sshConfigFile
 
 if ( $null -ne $TrustedHostCA ) {
     if ( -not ( Test-Path -Path $HOME/.ssh/known_hosts ) -or -not ( Select-String -Path $HOME/.ssh/known_hosts "$TrustedHostCA" -SimpleMatch ) ) {
-        Write-Host "Added domain trusted host CA to ${HOME}/.ssh/known_hosts"
+        Write-Host "+ Added domain trusted host CA to ${HOME}/.ssh/known_hosts."
         @"
 # trusted SSH CA for $Domain
 $TrustedHostCA
@@ -235,5 +244,11 @@ $TrustedHostCA
     }
 }
 
-Write-Host "Setup completed OK."
+$publicKey = Get-Content -Path $HOME/.ssh/id_ed25519_${Domain}.pub
+
+if ( $true -eq $createdKeyPair ) {
+    Write-Host "+ Enrolling your SSH public key."
+    & ssh ${Login}@sshenrol.$Domain "$publicKey"
+    Write-Host "+ Open a new powershell window to start using ssh."
+}
 
